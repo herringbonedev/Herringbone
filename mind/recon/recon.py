79912bde0d346741b45c00d639037381229da7c0
unixv <58447354+unixv@users.noreply.github.com>
2025-06-11 13:56:06 -0500

    Add files via upload

--- mind/recon/recon.py
+++ mind/recon/recon.py
@@ -9,14 +9,20 @@ OLLAMA_URL = 'http://localhost:11434/api/generate'
 PROMPT_TEMPLATE = """
 You are a cybersecurity recon analyst. Given the log below, answer this question in a concise and technical manner using the following format:
 
-You must remember. This will be read and processed by another program. Don't use any human communication. Just give the answer.
-
-Don't say things like: Here's my revaluation or anything. Only use the keys I've given.
+You need to 
 
 "log_type":"Your evaluation of what operating system, application or whatever kind of source this log came from",
 "origin":"The country of origin that this log came from",
 "description":"a description of what this log is",
 "malicious":"Your evaluation if it is malicious or not from a scale of 1 (least malicious) to 100 (most malicious)"
+
+
+here is an example of a perfect answer:
+
+"log_type":"Windows 10",
+"origin":"United States",
+"description":"Login event",
+"malicious":"10"
 
 Heres the log to evaluate: {record}
 """
