55a34e3eeeb64519bd60fd515d5f6273108da6ef
unixv <andrew@herringbone.dev>
2025-08-03 15:13:13 -0500

    apps API shows herringbone namespace deployments

--- herringbone/apps/app.py
+++ herringbone/apps/app.py
@@ -3,13 +3,30 @@ import kube_functions
 
 app = Flask(__name__)
 
+image_comp = {
+    "quay.io/herringbone/enrichment:latest": "enrichment",
+    "quay.io/herringbone/herringbone-apps:latest": "herringbone-apps",
+    "quay.io/herringbone/mind_recon:latest": "mind_recon",
+    "quay.io/herringbone/receiver:latest": "receiver",
+}
+
 @app.route('/herringbone/apps/status', methods=['GET'])
 def apps_status():
+    """Get the status of Herringbone apps"""
     k8s_admin = kube_functions.KubernetesAppAdmin()
-    pods = k8s_admin.get_pods()
-    services = k8s_admin.get_services()
     deployments = k8s_admin.get_deployments()
-    return jsonify({"pods": pods, "services": services, "deployments": deployments})
+
+    status = {}
+    for deployment in deployments:
+        if deployment["images"][0] in list(image_comp.keys()):
+            status[image_comp[deployment["images"][0]]] = "Deployed"
+
+    return jsonify(status)
+
+@app.route('/herringbone/apps/ready')
+def apps_ready():
+
+    return jsonify({"ready":True, "description":"herringbone/apps API is ready."})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=7002)
