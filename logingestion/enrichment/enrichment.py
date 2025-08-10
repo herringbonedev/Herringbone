253426b621cd19a66489720f2cc503e1cb59e9e3
unixv <andrew@herringbone.dev>
2025-08-10 09:34:38 -0500

    start working on pipeline

--- logingestion/enrichment/enrichment.py
+++ logingestion/enrichment/enrichment.py
@@ -79,6 +79,7 @@ while True:
     try:
         enrichment_result = perform_recon(doc["raw_log"])
         if os.environ.get("ENRICHMENT_SVC") != "test.svc":
+            print("[→] Inserting log into MongoDB")
             collection.update_one(
                 {"_id": doc["_id"]},
                 {
@@ -92,6 +93,7 @@ while True:
             print(f"[✓] Enriched log {doc['_id']}")
     except Exception as e:
         if os.environ.get("ENRICHMENT_SVC") != "test.svc":
+            print("[→] Inserting log into MongoDB")
             collection.update_one(
                 {"_id": doc["_id"]},
                 {"$set": {"recon": False}}
