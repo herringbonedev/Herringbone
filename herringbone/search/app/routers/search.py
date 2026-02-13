9dd178acd2765c5bc8f9e58efd64baecd4f0d8fa
unixv <andrewcampagnaservices@gmail.com>
2026-02-13 16:54:19 -0600

    missed one

--- herringbone/search/app/routers/search.py
+++ herringbone/search/app/routers/search.py
@@ -8,7 +8,7 @@ from modules.database.mongo_db import HerringboneMongoDatabase
 from modules.auth.user import get_current_user
 from app.service import search_collection_service, get_collection_fields
 
-from config import (
+from app.config import (
     MAX_LIMIT,
     MAX_SCHEMA_SAMPLE,
     MAX_SCHEMA_DEPTH,
