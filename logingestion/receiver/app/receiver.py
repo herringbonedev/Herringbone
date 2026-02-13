30ffc968afea84e99efeff0abcbe077d55a4588c
unixv <andrewcampagnaservices@gmail.com>
2026-02-13 16:30:04 -0600

    fix path

--- logingestion/receiver/app/receiver.py
+++ logingestion/receiver/app/receiver.py
@@ -4,19 +4,19 @@ TYPE = os.environ.get("RECEIVER_TYPE")
 print("Starting herringbone receiver..."+ TYPE)
 
 if TYPE == "UDP":
-    import inet
+    import app.inet
     inet.start_udp_receiver()
 
 elif TYPE == "TCP":
-    import inet
+    import app.inet
     inet.start_tcp_receiver()
 
 elif TYPE == "HTTP":
-    import web
+    import app.web
     web.start_http_receiver()
 
 elif TYPE == "REMOTE":
-    import remote
+    import app.remote
     remote.start_remote_receiver()
 
 else:
