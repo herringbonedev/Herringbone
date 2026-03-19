ee64fd801516ffa16e4d4fd87f0398ada994341e
unixv <andrewcampagnaservices@gmail.com>
2026-03-19 09:42:04 -0500

    fix

--- herringbone/auth/app/main.py
+++ herringbone/auth/app/main.py
@@ -19,6 +19,5 @@ app.include_router(auth.router)
 try:
     from app.enterprise import register_enterprise
     register_enterprise(app)
-    app.state.single_tenant = False
 except ImportError:
-    app.state.single_tenant = True
+    print("Core (Free)")
